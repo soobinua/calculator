c7e0fff4bd45fb18da84c31aa00d16f0539c5248
J <Dev7er@gmail.com>
2023-07-03 21:30:49 +0900

    input update - keyboard, file

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,6 +1,11 @@
 package calculator;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Calculator {
 
@@ -8,19 +13,40 @@ public class Calculator {
 
 		Input inputClass = new Input();
 
-//		String fileInput = inputClass.getFileInput();
-//		System.out.println("파일 내용 : " + fileInput);
-
-		String input = inputClass.getInput();
-//		System.out.println("입력하신 내용 : " + input);
-
-		Lexer lexer = new Lexer(input);
-		TokenQueue tokenQueue = lexer.lex();
-
-		while (!tokenQueue.isEmpty()) {
-			System.out.println("token : " + tokenQueue.deQueue().getLiteral());
+		//File create and write
+//		try {
+//			Files.writeString(Paths.get("xyz.txt"), "calculator", StandardCharsets.UTF_8);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		
+		//File input
+//		File file = new File("abc.txt");
+//		Scanner fileScanner = new Scanner(file);
+		
+		//KeyBoard input
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("계산할 수식을 입력하세요. (종료 : q)");
+		while(true) {
+//			String input = inputClass.getFileInput(file, fileScanner);
+//			System.out.println("파일 내용 : " + input);
+			String input = inputClass.getInput(scanner);
+			
+			if(input.equals("q")) {
+				break;
+			}
+			
+			Lexer lexer = new Lexer(input);
+			TokenQueue tokenQueue = lexer.lex();
+			
+			while (!tokenQueue.isEmpty()) {
+				System.out.println("token : " + tokenQueue.deQueue().getLiteral());
+			}
 		}
+//		fileScanner.close();
+		scanner.close();
 
+		System.out.println("입력이 종료되었습니다.");
 	}
 
 }
--- src/calculator/Lexer.java
+++ src/calculator/Lexer.java
@@ -9,23 +9,14 @@ public class Lexer {
 	}
 
 	public TokenQueue lex() {
-		// input = 2 + 3 * 4;
-
+		
 		TokenQueue tokenQueue = new TokenQueue();
 		// lexical analysis 어휘 분석
 		// 결과 : 여러개 token 나온다.
-		Token token = new Token("4", TokenType.OPERAND);
+		Token token = new Token(input, TokenType.OPERAND);
 		tokenQueue.enQueue(token);
-		token = new Token("5", TokenType.OPERAND);
-		tokenQueue.enQueue(token);
-		token = new Token("+", TokenType.OPERATOR);
-		tokenQueue.enQueue(token);
-		token = new Token("-", TokenType.OPERATOR);
-		tokenQueue.enQueue(token);
-		token = new Token("*", TokenType.OPERATOR);
-		tokenQueue.enQueue(token);
-
 		return tokenQueue;
+		
 	}
 
 }
