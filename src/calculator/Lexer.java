06e8a289b5ad604b4b392b0517f2863f358603f5
soobinua <Dev7er@gmail.com>
2023-07-08 13:11:58 +0900

    String input을 String[] input 배열로 수정

--- src/calculator/Lexer.java
+++ src/calculator/Lexer.java
@@ -13,7 +13,12 @@ public class Lexer {
 		TokenQueue tokenQueue = new TokenQueue();
 		// lexical analysis 어휘 분석
 		// 결과 : 여러개 token 나온다.
-		Token token = new Token(input, TokenType.OPERAND);
+		String[] inputArr = input.split(""); //구분자 "" 빈문자열
+		String tokenInput = "";
+		for(int i = 0; i <= inputArr.length - 1; i++) {
+			tokenInput = inputArr[i];
+		}
+		Token token = new Token(tokenInput, TokenType.OPERAND);
 		tokenQueue.enQueue(token);
 		return tokenQueue;
 		
