8ee248e9768a361d97f270ffe729b5aa3316f0d7
soobinua <Dev7er@gmail.com>
2023-07-12 21:38:06 +0900

    토큰 생성 for문 while로 수정

--- src/calculator/Lexer.java
+++ src/calculator/Lexer.java
@@ -9,21 +9,35 @@ public class Lexer {
 
 	public Lexer(String input) {
 		this.input = input;
-		this.position = -1;
+		this.position = 0;
+
+		if (input.length() == 0) {
+			throw new RuntimeException();
+		} else {
+			readCharacter();
+		}
 	}
 
-	public void next() {
-		// position++; 읽지 않았는데 position 값 증가하고 싶지 않다.
-		if (position > input.length() - 2) { // position 위치에 따라 length() - 1
-			// 마지막 인덱스 초과 읽을 수 없다.
-			throw new RuntimeException();
+	public void readCharacter() {
+		if (position > input.length() - 1) {
+			charInput = ';';
+		} else {
+			charInput = input.charAt(position);
+			index = position;
+			position++;
 		}
-		position++;
-		charInput = input.charAt(position);
+	}
+
+	public void peekCharacter() {
+		if (position > input.length() - 1) {
+			charInput = ';';
+		} else {
+			charInput = input.charAt(position);
+			position++;
+		}
 	}
 
 	public TokenQueue lex() {
-
 		TokenQueue tokenQueue = new TokenQueue();
 		// lexical analysis 어휘 분석
 		// 결과 : 여러개 token 나온다.
@@ -34,36 +48,30 @@ public class Lexer {
 //		}
 
 		Token token = null;
-		char tokenInput;
-		for (int i = 0; i <= input.length() - 1; i++) {
-			tokenInput = input.charAt(i); // charAt(index)
-			// TODO
-			// 현재 한 자릿수 토큰은 생성 가능하나 두자릿수부터 토큰 생성 불가
-			// token 생성하기 전에 다음 문자를 읽고 판단한 뒤 토큰 생성해야한다.
-			// switch문 안에서 ' '체크가 아닌 이전에 공백문자 체크 후 다음 문자 읽고 진행.
-			// tab키도 체크하지 않고 있다.
-			switch (tokenInput) {
+		while (charInput != ';') {
+			switch (charInput) {
 			case '+':
-				token = new Token(String.valueOf(tokenInput), TokenType.OPERATOR);
+				token = new Token(String.valueOf(charInput), TokenType.OPERATOR);
 				break;
 			case '-':
-				token = new Token(String.valueOf(tokenInput), TokenType.OPERATOR);
+				token = new Token(String.valueOf(charInput), TokenType.OPERATOR);
 				break;
 			case '*':
-				token = new Token(String.valueOf(tokenInput), TokenType.OPERATOR);
+				token = new Token(String.valueOf(charInput), TokenType.OPERATOR);
 				break;
 			case '/':
-				token = new Token(String.valueOf(tokenInput), TokenType.OPERATOR);
+				token = new Token(String.valueOf(charInput), TokenType.OPERATOR);
 				break;
 			case ' ': // 빈문자열은 가능하나 빈문자 불가능. 빈문자 표현 = '\0' (이스케이프문자)
 				token = null;
 				break; // 스페이스바 저장 X
 			default:
-				token = new Token(String.valueOf(tokenInput), TokenType.OPERAND);
+				token = new Token(String.valueOf(charInput), TokenType.OPERAND);
 			}
 			if (token != null) {
 				tokenQueue.enQueue(token);
 			}
+			readCharacter();
 		}
 		return tokenQueue;
 
